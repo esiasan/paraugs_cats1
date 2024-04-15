2f69e68bc897cede89150bbf9b0fe381e39ff041
esiasan <fridihs.alex@gmail.com>
2024-04-15 15:40:02 +0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+//console.log(1)
+function sutitzinu()
+{
+    console.log('sutitzinu()darbojas');
+    let zina = document.querySelector('zina').value;
+    let zinas = documet.querySelector('catazina');
+    zinas.innerHTML = zinas.innerHTML+ '<br>'zina.value;
+}
